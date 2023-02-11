f52d279d656d0ccfb2374f1761acc040069c9791
EviLinArm <zakharenko07@gmail.com>
2023-02-11 13:43:37 +1000

    Vue TS init!

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import TheNavbar from './components/TheNavbar.vue'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  components: {
+    TheNavbar,
+    TheFooter,
+  },
+  data: () => ({
+    tagline: 'All what you want to eat!',
+  }),
+}
+</script>
+
+<template>
+  <TheNavbar :tagline="tagline" />
+  <RouterView />
+  <TheFooter />
+</template>
+
+<style></style>
