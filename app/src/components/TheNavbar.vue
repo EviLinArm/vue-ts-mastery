d9436d7d46d6edfab341234b1aebaff79532f99e
EviLinArm <zakharenko07@gmail.com>
2023-02-13 13:44:53 +1000

    Props and emits TS added!

--- app/src/components/TheNavbar.vue
+++ app/src/components/TheNavbar.vue
@@ -1,51 +1,50 @@
-<script>
-export default {
-  props: {
+<script setup lang="ts">
+import { ref } from "vue"
+
+const props = defineProps({
     tagline: {
-      type: [String, Number],
-      default: 'String or Number',
-      required: false,
+        type: [String, Number],
+        default: "String or Number",
+        required: false
+    }
+})
+
+const navList = ref([
+    {
+        name: "Home",
+        path: "/"
     },
-  },
-  data: () => ({
-    navList: [
-      {
-        name: 'Home',
-        path: '/',
-      },
-      {
-        name: 'Restaurants',
-        path: '/restaurants',
-      },
-      {
-        name: 'Dishes',
-        path: '/dishes',
-      },
-    ],
-  }),
-}
+    {
+        name: "Restaurants",
+        path: "/restaurants"
+    },
+    {
+        name: "Dishes",
+        path: "/dishes"
+    }
+])
 </script>
 
 <template>
-  <nav class="navbar has-shadow">
-    <div class="navbar-brand">
-      <RouterLink to="/" class="navbar-item">
-        <img src="@/assets/to-eat-logo.svg" alt="" width="100" />
-      </RouterLink>
-    </div>
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <div class="navbar-item">
-          <small>{{ tagline }}</small>
+    <nav class="navbar has-shadow">
+        <div class="navbar-brand">
+            <RouterLink to="/" class="navbar-item">
+                <img src="@/assets/to-eat-logo.svg" alt="" width="100" />
+            </RouterLink>
         </div>
-      </div>
-      <div class="navbar-end">
-        <div v-for="navItem in navList" class="navbar-item" :key="`nav-${navItem.name}`">
-          <RouterLink :to="navItem.path">{{ navItem.name }}</RouterLink>
+        <div class="navbar-menu">
+            <div class="navbar-start">
+                <div class="navbar-item">
+                    <small>{{ tagline }}</small>
+                </div>
+            </div>
+            <div class="navbar-end">
+                <div v-for="navItem in navList" class="navbar-item" :key="`nav-${navItem.name}`">
+                    <RouterLink :to="navItem.path">{{ navItem.name }}</RouterLink>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 </template>
 
 <style></style>
